fd1eec9118e9efba2a7d11061a325e2fba0b801f
Gabriel Ribeiro <gabriel202ribeiro@gmail.com>
2023-10-18 18:41:22 -0300

    ajustei a validação dos dados dentro do formulario

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 const botaoLogin = document.querySelector('button');
 const inputs = document.querySelectorAll('input');
 const form = document.querySelector('#acesso')
+const dados = {};
 
 function previnirPadrao(event) {
     event.preventDefault();
 }
 
 function capiturandoDados(event) {
-    event.preventDefault();
-
-    const name = document.querySelector('#name');
-    const email = document.querySelector('#email');
-    const senha = document.querySelector('#senha');
-
-    console.log(name.value)
-    console.log(email.value)
-    console.log(senha.value)
+    dados[event.target.name] = event.target.value;
 }
 
 form.addEventListener('change', capiturandoDados)
@@ -25,10 +18,10 @@ inputs.forEach((input) => {
         if (input.value !== "") {
             input.classList.toggle("com-dados");
         } else {
-            input.classList.toggle("sem-dados");
             input.classList.remove("com-dados");
+            input.classList.toggle("sem-dados");
             const mensagem = document.querySelector('.erro');
-            mensagem.textContent = "Preencha o campo corretamente.";
+            mensagem.textContent = "Preencha os campos corretamente.";
         }
     })
 })
